7a562f7cd44ca52280be2f822342bd912ba56467
Eric Wolf <ericwolf42@gmail.com>
2019-11-29 17:28:32 +0100

    sdl-try-drivers: first working version
    it just tries all the reported drivers, wether renderers can be
    created with them and reports so or reports the error message.

--- Main.hs
+++ Main.hs
@@ -1,3 +1,45 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-main = putStrLn "Hello World!"
+import Foreign.C.Types (CInt)
+import Control.Monad.IO.Class (MonadIO)
+import Control.Exception (catch, SomeException)
+
+import Text.Pretty.Simple (pPrintNoColor)
+import SDL
+
+myPPrint :: (Show a, MonadIO m) => a -> m ()
+myPPrint = pPrintNoColor
+
+checkRenderDriver :: Window -> (CInt, RendererInfo) -> IO ()
+checkRenderDriver window driver = do
+  maybeRenderer <- tryRenderDriver window driver
+  case maybeRenderer of
+    (Just renderer) -> destroyRenderer renderer
+    Nothing -> return ()
+
+tryRenderDriver :: Window -> (CInt, RendererInfo) -> IO (Maybe Renderer)
+tryRenderDriver window (idx, info) = catch (do
+        putStrLn "== trying driver:"
+        myPPrint info
+        renderer <- createRenderer window idx defaultRenderer
+        putStrLn "** success"
+        return (Just renderer))
+        (\e -> do
+                  putStrLn ("-- couldn't load driver number "  ++ show idx ++ ":")
+                  myPPrint (e :: SomeException)
+                  return Nothing)
+
+checkRenderDrivers :: Window -> IO ()
+checkRenderDrivers window = do
+  drivers <- zip [0 ..] <$> getRenderDriverInfo
+  putStrLn ("there are" ++ show (length drivers) ++ " drivers to check")
+  mapM_ (checkRenderDriver window) drivers
+
+main :: IO ()
+main = do
+  initializeAll
+  window <- createWindow "sdl-try-drivers" defaultWindow {windowResizable = True}
+  checkRenderDrivers window
+  destroyWindow window
+
--- default.nix
+++ default.nix
@@ -1,11 +1,11 @@
-{ mkDerivation, base, pretty-simple, sdl2, stdenv }:
+{ mkDerivation, base, pretty-simple, sdl2, stdenv, text }:
 mkDerivation {
   pname = "sdl-try-drivers";
   version = "0.0.0.1";
   src = ./.;
   isLibrary = false;
   isExecutable = true;
-  executableHaskellDepends = [ base pretty-simple sdl2 ];
+  executableHaskellDepends = [ base pretty-simple sdl2 text ];
   description = "small testing tool for sdl2 and accelerated drivers";
   license = stdenv.lib.licenses.mit;
 }
